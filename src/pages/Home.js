8af202ab67d73b9d77194fd36a113ccf1fe90701
Kuldeep Bhimte <kuldeip@Kuldeeps-MacBook-Pro.local>
2019-07-29 18:02:53 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+import PostForm from '../components/PostForm';
+
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+    const [limit, setLimit] = useState(5);
+    const [editingPost, setEditingPost] = useState({title: '', body: '', id: null});
+    
+    useEffect(() => {
+        axios.get('/posts').then((res) => {
+            console.log(res.data);
+            setPosts(res.data);
+        }).catch((error) => {
+            console.error(error);
+        })
+    }, []);
+
+    function editPost(post) {
+        console.log(post);
+        setEditingPost(post);
+
+    }
+
+    function deletePost(id) {
+        axios.delete(`/post/${id}`).then(() => {
+            const updatedPosts = posts.filter(p => p.id !== id);
+            setPosts(updatedPosts);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    const getNumberOfPosts = () => {
+        axios.get(`/posts/${limit}`).then(res => {
+            setPosts(res.data)
+        }).catch(err => {
+            console.error(err);
+        })
+    }
+    const addPost = (post) => {
+
+        if(posts.find(p => p.id === post.id)){
+            const index = posts.findIndex(p => p.id === post.id);
+            const updatedPosts = [...posts];
+            updatedPosts.splice(index, 1, post);
+            setPosts(updatedPosts);
+        } else {
+            const updatedPosts = [...posts, post];
+            setPosts(updatedPosts);
+        }
+
+        
+    }
+
+    return (
+        <div>
+            <div className='row'>
+                <div className='col s6'>
+                    <PostForm addPost={addPost} editingPost={editingPost} />
+                </div>
+                <div className="col s3 push-in">
+                    <p>
+                        Limit number of posts
+                    </p>
+                    <input type="number" value={limit} name="limit" onChange={event => setLimit(event.target.value)} />
+                    <button className="waves-effect waves-light btn" onClick={getNumberOfPosts}>
+                        Set
+                    </button>
+                </div>
+            </div>
+            
+            <div className='row'>
+                {
+                    posts.map(post => (
+                        <div className='col s6' key={post.id}>
+                            <div className='card'>
+                                <div className='card-content'>
+
+                                
+                                <div className='card-title'>
+                                    {post.title}
+                                </div>
+                                <p className='timestamp'>
+                                    {post.createdAt}
+                                </p>
+                                <p className='card-body'>
+                                    {post.body}
+                                </p>
+                                
+                                </div>
+                                <div className='card-action'> 
+                                    <a onClick={editPost.bind(null, post)}>
+                                        Edit
+                                    </a>
+                                    <a className='delete-btn' onClick={deletePost.bind(null, post.id)}>
+                                        Delete
+                                    </a>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
